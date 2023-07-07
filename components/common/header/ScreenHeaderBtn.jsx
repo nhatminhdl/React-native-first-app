cc13023815320e18e4c23859b2e510cd958ae741
Mr.Minh <thinhatminhdl@gmail.com>
2023-07-07 11:58:38 +0700

    [07-07-2023]

--- components/common/header/ScreenHeaderBtn.jsx
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import {TouchableOpacity, Image} from 'react-native'
 
 import styles from './screenheader.style'
 
-const ScreenHeaderBtn = () => {
+const ScreenHeaderBtn = (iconUrl) => {
   return (
-    <View>
-      <Text>ScreenHeaderBtn</Text>
-    </View>
+    <TouchableOpacity>
+      <Image
+        source={iconUrl}
+      />
+    </TouchableOpacity>
   )
 }
 
